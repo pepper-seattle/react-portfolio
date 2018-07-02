7ebafc84f1e44f5cbfa1bbda283b0c6976bdcbbc
Anne O'Malley <pepper.seattle@gmail.com>
2018-07-02 14:27:57 -0700

    creation of basic light and dark theme files

---
+++ portfolio/src/themes/dark.js
@@ -0,0 +1,9 @@
+export const colors = {
+  background: '#222',
+  primary: '#407FC7',
+  text: '#e9e9e9',
+};
+
+export default {
+  colors,
+};
---
+++ portfolio/src/themes/light.js
@@ -0,0 +1,9 @@
+export const colors = {
+  background: '#e9e9e9',
+  primary: '#407FC7',
+  text: '#222',
+};
+
+export default {
+  colors,
+};
